3863739af8267064cf5241645093ea4b4b043ac1
Jerry Yee <jeyee@JEYEE-WS.cisco.com>
2014-07-19 17:04:12 -0400

    yo

--- NavigationDrawer/src/it/gmariotti/android/example/navigationdrawer/NsMenuAdapter.java
+++ NavigationDrawer/src/it/gmariotti/android/example/navigationdrawer/NsMenuAdapter.java
@@ -13,125 +13,112 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *******************************************************************************/
+
 package it.gmariotti.android.example.navigationdrawer;
 
-
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class NsMenuAdapter extends ArrayAdapter<NsMenuItemModel> {
 
 	/*
-	 * public NsMenuAdapter(Context context, int resource, int
+     * public NsMenuAdapter(Context context, int resource, int
 	 * textViewResourceId, String[] objects) { super(context,
 	 * R.layout.ns_menu_row, textViewResourceId, objects); }
 	 */
 
-	public NsMenuAdapter(Context context) {
-		super(context, 0);
-	}
+    public NsMenuAdapter(Context context) {
+        super(context, 0);
+    }
 
-	public void addHeader(int title) {
-		add(new NsMenuItemModel(title, -1, true));
-	}
+    public void addHeader(int title) {
+        add(new NsMenuItemModel(title, -1, true));
+    }
 
-	public void addItem(int title, int icon) {
-		add(new NsMenuItemModel(title, icon, false));
-	}
+    public void addItem(int title, int icon) {
+        add(new NsMenuItemModel(title, icon, false));
+    }
 
-	public void addItem(NsMenuItemModel itemModel) {
-		add(itemModel);
-	}
+    public void addItem(NsMenuItemModel itemModel) {
+        add(itemModel);
+    }
 
-	@Override
-	public int getViewTypeCount() {
-		return 2;
-	}
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
 
-	@Override
-	public int getItemViewType(int position) {
-		return getItem(position).isHeader ? 0 : 1;
-	}
+    @Override
+    public int getItemViewType(int position) {
+        return getItem(position).isHeader ? 0 : 1;
+    }
 
-	@Override
-	public boolean isEnabled(int position) {
-		return !getItem(position).isHeader;
-	}
+    @Override
+    public boolean isEnabled(int position) {
+        return !getItem(position).isHeader;
+    }
 
-	public static class ViewHolder {
-		public final TextView textHolder;
-		public final ImageView imageHolder;
-		public final TextView textCounterHolder;
+    public static class ViewHolder {
+        public final TextView textHolder;
+        public final ImageView imageHolder;
+        public final TextView textCounterHolder;
 
-		public ViewHolder(TextView text1, ImageView image1,TextView textcounter1) {
-			this.textHolder = text1;
-			this.imageHolder = image1;
-			this.textCounterHolder=textcounter1;
-		}
-	}
+        public ViewHolder(TextView text1, ImageView image1, TextView textcounter1) {
+            this.textHolder = text1;
+            this.imageHolder = image1;
+            this.textCounterHolder = textcounter1;
+        }
+    }
 
-	public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View convertView, ViewGroup parent) {
 
-		NsMenuItemModel item = getItem(position);
-		ViewHolder holder = null;
-		View view = convertView;
+        NsMenuItemModel item = getItem(position);
+        View view = convertView;
 
-		if (view == null) {
-			int layout = R.layout.ns_menu_row_counter;
-			if (item.isHeader)
-				layout = R.layout.ns_menu_row_header;
-
-			view = LayoutInflater.from(getContext()).inflate(layout, null);
-
-			TextView text1 = (TextView) view.findViewById(R.id.menurow_title);
-			ImageView image1 = (ImageView) view.findViewById(R.id.menurow_icon);
-			TextView textcounter1 = (TextView) view.findViewById(R.id.menurow_counter);
-			view.setTag(new ViewHolder(text1, image1,textcounter1));
-		}
-
-		if (holder == null && view != null) {
-			Object tag = view.getTag();
-			if (tag instanceof ViewHolder) {
-				holder = (ViewHolder) tag;
-			}
-		}
-
-
-		
-	    if(item != null && holder != null)
-	    {
-	    	if (holder.textHolder != null)
-				holder.textHolder.setText(item.title);
-            if (getItem(position).type == 1 && !item.isHeader) {
-                view.findViewById(R.id.menurow_counter).setVisibility(View.GONE);
+        if (view == null) {
+            int layout = R.layout.ns_menu_row_counter;
+            if (item.isHeader) {
+                layout = R.layout.ns_menu_row_header;
             }
 
-            if (holder.textCounterHolder != null){
-	    		if (item.counter > 0){
-	    			holder.textCounterHolder.setVisibility(View.VISIBLE);
-	    			holder.textCounterHolder.setText(""+item.counter);
-	    		}else{
-	    			holder.textCounterHolder.setVisibility(View.GONE);
-				}
-			}
-	    	
-	        if (holder.imageHolder != null) {
-				if (item.iconRes > 0) {
-					holder.imageHolder.setVisibility(View.VISIBLE);
-					holder.imageHolder.setImageResource(item.iconRes);
-				} else {
-					holder.imageHolder.setVisibility(View.GONE);
-				}
-			}
+            view = LayoutInflater.from(getContext()).inflate(layout, null);
+        }
 
-	    }
-	    
-	    return view;		
-	}
+        if (item.type == 1 && !item.isHeader) {
+            Log.d("", "SWITCH");
+            Switch switchButton = (Switch) view.findViewById(R.id.menurow_switch);
+            switchButton.setVisibility(View.VISIBLE);
+        }
+
+        TextView title = (TextView) view.findViewById(R.id.menurow_title);
+        title.setText(item.title);
+        title.setVisibility(View.VISIBLE);
+
+        if (!item.isHeader) {
+            TextView counter = (TextView) view.findViewById(R.id.menurow_counter);
+            if (counter != null) {
+                counter.setText("" + item.counter);
+                counter.setVisibility(View.VISIBLE);
+            }
+
+
+
+
+            ImageView icon = (ImageView) view.findViewById(R.id.menurow_icon);
+            if (icon != null) {
+                icon.setImageResource(item.iconRes);
+                icon.setVisibility(View.VISIBLE);
+            }
+        }
+
+        return view;
+    }
 
 }
