738d55c90175028d43a31b945ad92fa569819319
Jerry Yee <yee@gatech.edu>
2014-07-21 00:59:57 -0400

    yes

--- NavigationDrawer/src/it/gmariotti/android/example/navigationdrawer/MainActivity.java
+++ NavigationDrawer/src/it/gmariotti/android/example/navigationdrawer/MainActivity.java
@@ -19,21 +19,25 @@ import android.app.Activity;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends Activity {
 
-    	private ListView mDrawerList;
-	private DrawerLayout mDrawer;
+    ViewFlipper mCCFlipper;
+    LinearLayout mAppearancePage;
+    ViewFlipper ccFlipper ;
+    private DrawerLayout mDrawer;
     private CustomActionBarDrawerToggle mDrawerToggle;
-	private String[] menuItems;
+    int a = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,66 +48,78 @@ public class MainActivity extends Activity {
         getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setHomeButtonEnabled(true);
 //
-		mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//
-//		// set a custom shadow that overlays the main content when the drawer
-//		// opens
-//		mDrawer.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-//
-//		_initMenu();
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawer.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
         mDrawerToggle = new CustomActionBarDrawerToggle(this, mDrawer);
-		mDrawer.setDrawerListener(mDrawerToggle);
+        mDrawer.setDrawerListener(mDrawerToggle);
+        mCCFlipper = (ViewFlipper) findViewById(R.id.ccFlipper);
+
+        mAppearancePage = (LinearLayout) findViewById(R.id.cc_appearance);
+        mAppearancePage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                mCCFlipper.showNext();
+            }
+        });
+
+        Animation slide_in_right = AnimationUtils.loadAnimation(this, R.anim.slide_in_right);
+        Animation slide_out_left = AnimationUtils.loadAnimation(this, R.anim.slide_out_left);
+
+        mCCFlipper.setInAnimation(slide_in_right);
+        //set the animation for the view leaving th screen
+        mCCFlipper.setOutAnimation(slide_out_left);
+
 
     }
 
-    private void _initMenu() {
-		NsMenuAdapter mAdapter = new NsMenuAdapter(this);
-
-		// Add Header
-		mAdapter.addHeader(R.string.ns_menu_main_header);
-
-		// Add first block
-
-		menuItems = getResources().getStringArray(
-				R.array.ns_menu_items);
-		String[] menuItemsIcon = getResources().getStringArray(
-				R.array.ns_menu_items_icon);
-
-		int res = 0;
-		for (String item : menuItems) {
-
-			int id_title = getResources().getIdentifier(item, "string",
-					this.getPackageName());
-			int id_icon = getResources().getIdentifier(menuItemsIcon[res],
-					"drawable", this.getPackageName());
-            int id_type = getResources().obtainTypedArray(R.array.ns_menu_types).getInt(res, 0);
-
-			NsMenuItemModel mItem = new NsMenuItemModel( id_title,  id_icon,false, 90, id_type);
-			mAdapter.addItem(mItem);
-			res++;
-		}
-
-		mAdapter.addHeader(R.string.ns_menu_main_header2);
-
-		mDrawerList = (ListView) findViewById(R.id.drawer);
-		if (mDrawerList != null)
-			mDrawerList.setAdapter(mAdapter);
-
-		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-
-    }
+//    private void _initMenu() {
+//        NsMenuAdapter mAdapter = new NsMenuAdapter(this);
+//
+//        // Add Header
+//        mAdapter.addHeader(R.string.ns_menu_main_header);
+//
+//        // Add first block
+//
+//        menuItems = getResources().getStringArray(
+//                R.array.ns_menu_items);
+//        String[] menuItemsIcon = getResources().getStringArray(
+//                R.array.ns_menu_items_icon);
+//
+//        int res = 0;
+//        for (String item : menuItems) {
+//
+//            int id_title = getResources().getIdentifier(item, "string",
+//                    this.getPackageName());
+//            int id_icon = getResources().getIdentifier(menuItemsIcon[res],
+//                    "drawable", this.getPackageName());
+//            int id_type = getResources().obtainTypedArray(R.array.ns_menu_types).getInt(res, 0);
+//
+//            NsMenuItemModel mItem = new NsMenuItemModel(id_title, id_icon, false, 90, id_type);
+//            mAdapter.addItem(mItem);
+//            res++;
+//        }
+//
+//        mAdapter.addHeader(R.string.ns_menu_main_header2);
+//
+//        mDrawerList = (ListView) findViewById(R.id.drawer);
+//        if (mDrawerList != null)
+//            mDrawerList.setAdapter(mAdapter);
+//
+//
+//    }
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         // Sync the toggle state after onRestoreInstanceState has occurred.
-		mDrawerToggle.syncState();
+        mDrawerToggle.syncState();
     }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-		mDrawerToggle.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
@@ -125,54 +141,41 @@ public class MainActivity extends Activity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         /*
-		 * The action bar home/up should open or close the drawer.
+         * The action bar home/up should open or close the drawer.
 		 * ActionBarDrawerToggle will take care of this.
 		 */
-		if (mDrawerToggle.onOptionsItemSelected(item)) {
-			return true;
-		}
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
 
         // Handle your other action bar items...
         return super.onOptionsItemSelected(item);
     }
 
-	private class CustomActionBarDrawerToggle extends ActionBarDrawerToggle {
+    private class CustomActionBarDrawerToggle extends ActionBarDrawerToggle {
 
-		public CustomActionBarDrawerToggle(Activity mActivity,DrawerLayout mDrawerLayout){
-			super(
-			    mActivity,
-			    mDrawerLayout,
-			    R.drawable.ic_drawer,
-			    R.string.ns_menu_open,
-			    R.string.ns_menu_close);
-		}
+        public CustomActionBarDrawerToggle(Activity mActivity, DrawerLayout mDrawerLayout) {
+            super(
+                    mActivity,
+                    mDrawerLayout,
+                    R.drawable.ic_drawer,
+                    R.string.ns_menu_open,
+                    R.string.ns_menu_close);
+        }
 
-		@Override
-		public void onDrawerClosed(View view) {
-			getActionBar().setTitle(getString(R.string.ns_menu_close));
-			invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-		}
+        @Override
+        public void onDrawerClosed(View view) {
+            getActionBar().setTitle(getString(R.string.ns_menu_close));
+            invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+        }
 
-		@Override
-		public void onDrawerOpened(View drawerView) {
-			getActionBar().setTitle(getString(R.string.ns_menu_open));
-			invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-		}
-	}
+        @Override
+        public void onDrawerOpened(View drawerView) {
+            getActionBar().setTitle(getString(R.string.ns_menu_open));
+            invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+        }
+    }
 
-	private class DrawerItemClickListener implements ListView.OnItemClickListener {
-
-		@Override
-		public void onItemClick(AdapterView<?> parent, View view, int position,
-				long id) {
-			// Highlight the selected item, update the title, and close the drawer
-			// update selected item and title, then close the drawer
-	        String text= "menu click... should be implemented";
-                    Toast.makeText(MainActivity.this, text, Toast.LENGTH_LONG).show();
-            //You should reset item counter
-
-		}
-
-	}
 
 }
